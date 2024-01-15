db731d6ace1d03e79c5d223e1c609f36907489b8
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2024-01-15 13:22:53 +0000

    HOTFIX: mobile fixing

--- components/home/HomeComponent.tsx
+++ components/home/HomeComponent.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Button from '../utils/Button';
 
 import { VisionType, visionIcons } from './config';
-import { BE_MEMBER_FORM_URL, DONATE_URL } from '../../utils/paths';
+import { BE_MEMBER_FORM_URL, DONATE_URL, GLOBAL_VILLAGE_URL } from '../../utils/paths';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useTranslation } from '../../i18n/client';
@@ -12,6 +12,8 @@ import { useMainAppContext } from '@/contexts/AppProvider';
 
 // Images
 import exterior1 from '@/public/images/training_center/exterior1.jpg';
+import heroPng from '@/public/images/frontpage/hero.png';
+import whoWeAreImage from '@/public/images/frontpage/quem_somos.png';
 
 const HomeComponent = () => {
   const { lng } = useMainAppContext();
@@ -22,8 +24,8 @@ const HomeComponent = () => {
       <section className='flex flex-col bg-bege-light px-8 pb-5 pt-10 lg:px-14 lg:pb-10 lg:pl-20 lg:pt-40'>
         <div className='mx-auto flex max-w-6xl flex-col gap-5 lg:flex-row lg:gap-1'>
           <div className='flex flex-col gap-2 lg:w-1/2'>
-            <h4 className='text-4xl font-black'>{t('home.title')}</h4>
-            <p className='text-justify'>{t('home.description')}</p>
+            <h1 className='font-black lg:text-4xl'>{t('home.title')}</h1>
+            <p className='lg:text-justify'>{t('home.description')}</p>
             <div className='mt-8 flex gap-5'>
               <div className='w-fit'>
                 <Button
@@ -46,10 +48,12 @@ const HomeComponent = () => {
           </div>
           <div className='relative mt-2 h-96 w-full py-20 lg:mt-0 lg:w-1/2 lg:py-0'>
             <Image
-              src='/images/frontpage/hero.png'
+              src={heroPng}
               fill={true}
               style={{ objectFit: 'contain' }}
               alt='Main Image Index'
+              loading='lazy'
+              sizes='100vw lg:50vw'
             />
           </div>
         </div>
@@ -62,10 +66,12 @@ const HomeComponent = () => {
         </div>
         <div className='relative h-96 w-full lg:w-1/2'>
           <Image
-            src='/images/frontpage/quem_somos.png'
+            src={whoWeAreImage}
             alt='quem_somos'
             fill={true}
             style={{ objectFit: 'cover' }}
+            loading='lazy'
+            sizes='100vw lg:50vw'
           />
         </div>
       </section>
@@ -78,24 +84,35 @@ const HomeComponent = () => {
           {visionIcons.map((option: VisionType, index: number) => (
             <div key={index} className='flex flex-col items-center gap-2'>
               <div>
-                <Suspense>
-                  <Image src={option.icon} alt={`${option.title} icon`} width={32} height={32} />
+                <Suspense fallback={<p>loading...</p>}>
+                  <Image
+                    src={option.icon}
+                    alt={`${option.title} icon`}
+                    width={32}
+                    height={32}
+                    sizes='32px'
+                  />
                 </Suspense>
               </div>
               <h5 className='font-black text-green'>{t(option.title)}</h5>
-              <h6 className='text-center'>{t(option.description)}</h6>
+              <p className='text-center'>{t(option.description)}</p>
             </div>
           ))}
         </div>
       </section>
       <section className='bg-bege-dark px-8 py-20 lg:px-20'>
         <div className='mx-auto flex max-w-6xl flex-col gap-5 lg:flex-row lg:gap-20'>
-          <Image src={exterior1} alt='Global Village' className='flex-1 rounded-lg lg:w-1/2' />
+          <Image
+            src={exterior1}
+            alt='Global Village'
+            className='flex-1 rounded-lg lg:w-1/2'
+            sizes='100vw lg:50vw'
+          />
           <div className='flex flex-1 flex-col gap-4 lg:w-1/2'>
             <h3 className='mt-5 font-black text-green'>Global Village</h3>
             <p>{t('home.global_village')}</p>
-            <Button type={'button'}>
-              <Suspense fallback=''>See More</Suspense>
+            <Button type={'button'} onClick={() => router.push(GLOBAL_VILLAGE_URL)}>
+              {t('see_more')}
             </Button>
           </div>
         </div>
