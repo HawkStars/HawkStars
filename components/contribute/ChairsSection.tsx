3fec796d27de52cf1fdfbe2edd1a41f3532838b7
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2024-03-27 00:45:32 +0000

    updating some stuff

--- components/contribute/ChairsSection.tsx
+++ components/contribute/ChairsSection.tsx
@@ -17,27 +17,45 @@ const ChairsSections = ({
   size,
   currentContributions = [],
 }: ChairsSectionsProps) => {
-  const missingContributions = Array(size - currentContributions.length).fill(null);
-
+  const missingContributionsLength = size - currentContributions.length;
+  const missingContributionsChairs = Array(missingContributionsLength).fill(null);
   return (
-    <div className='my-10 flex flex-col justify-center gap-3 lg:mx-0'>
-      <h3 className='mt-5 text-center'>{title}</h3>
-      {price && <h4 className='text-center font-black text-green'>{price}</h4>}
-      <div className='mx-auto mt-5 flex w-full flex-wrap justify-center gap-5 lg:w-2/3'>
-        {currentContributions.map((contribution) => {
-          return (
-            <ContributionInfoSection
-              key={contribution.id}
-              contributor={contribution.donor}
-              icon={iconFilled}
-            />
-          );
-        })}
-        {missingContributions.map((_, index) => {
-          return <ContributionInfoSection key={index} icon={icon} />;
-        })}
+    <>
+      <div className='my-10 hidden flex-col justify-center gap-3 lg:mx-0 lg:flex'>
+        <h3 className='mt-5 text-center'>{title}</h3>
+        {price && <h4 className='text-center font-black text-green'>{price}</h4>}
+        <div className='mx-auto mt-5 flex w-full flex-wrap justify-center gap-5 lg:w-2/3'>
+          {currentContributions.map((contribution) => {
+            return (
+              <ContributionInfoSection
+                key={contribution.id}
+                contributor={contribution.donor}
+                icon={iconFilled}
+              />
+            );
+          })}
+          {missingContributionsChairs.map((_, index) => {
+            return <ContributionInfoSection key={index} icon={icon} />;
+          })}
+        </div>
       </div>
-    </div>
+      <div className='my-10 flex flex-col justify-center gap-5 lg:hidden'>
+        <div className='flex flex-col gap-2 text-center'>
+          <h3 className='text-center'>{title}</h3>
+          {price && <h4 className='font-black text-green'>{price}</h4>}
+        </div>
+        <div className='flex flex-row gap-5 mx-auto'>
+          <div className='flex flex-col gap-2'>
+            {iconFilled}
+            <p className='text-center'>{currentContributions.length}</p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {icon}
+            <p className='text-center'>{missingContributionsLength}</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
