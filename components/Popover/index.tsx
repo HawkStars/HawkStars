8e64b854df5d93da988d0050819cd625dadc4caa
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2024-12-27 01:13:20 +0000

    improving the menu header

--- components/Popover/index.tsx
+++
@@ -1,1 +0,0 @@
-export { PopoverHoverer } from './PopoverHoverer';
--- components/menu/DropdownMenu.tsx
+++ components/menu/DropdownMenu.tsx
@@ -1,25 +1,14 @@
 'use client';
 
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Popover,
-  PopoverButton,
-  PopoverPanel,
-} from '@headlessui/react';
 import classNames from 'classnames';
 import { useRouter } from 'next/navigation';
 
-import { PiCaretDownThin, PiCaretRightThin } from 'react-icons/pi';
-import { Suspense, useRef, useState } from 'react';
+import { PiCaretDownThin } from 'react-icons/pi';
 
 import { useTranslation } from '@/i18n/client';
 import { transformUrl } from '@/utils/paths';
 import { NavbarUrlItem } from '../navbar/types';
 import { useLanguageCookie } from '@/contexts/AppProvider';
-// import { useLanguageCookie } from '@/hooks/useLanguageCookie';
 
 type DropdownMenuProps = {
   title: string;
@@ -32,43 +21,32 @@ const DropdownMenu = ({ title, options }: DropdownMenuProps) => {
   const router = useRouter();
 
   return (
-    <Popover className='group relative inline-block text-left'>
-      {({ open }) => (
-        <div onMouseEnter={() => handleEnter(open)} onMouseLeave={() => handleLeave(open)}>
-          <PopoverButton ref={triggerRef}>
-            <div className='flex gap-1'>
-              <h6>
-                <Suspense fallback=''>{t(title)}</Suspense>
-              </h6>
+    <div className='group relative cursor-pointer'>
+      <div className='flex gap-1'>
+        <h6>{t(title)}</h6>
 
-              {options && options.length > 0 && (
-                <div className='my-auto'>
-                  <PiCaretDownThin className='ease duration-300 group-data-[open]:-rotate-90' />
-                </div>
-              )}
-            </div>
-          </PopoverButton>
-          <PopoverPanel
-            anchor='bottom'
-            className='flex cursor-pointer flex-col gap-2 rounded bg-white'
+        {options && options.length > 0 && (
+          <div className='my-auto'>
+            <PiCaretDownThin className='ease transition-transform duration-500 group-hover:-rotate-90' />
+          </div>
+        )}
+      </div>
+      <div className='absolute -left-3 mt-1 hidden flex-col gap-2 rounded bg-white duration-500 ease-in group-hover:flex'>
+        {options.map((option, index) => (
+          <div
+            key={index}
+            className={classNames('w-fit whitespace-nowrap px-5 py-2', {
+              'text-neutral-400': !option.url || option.disabled,
+            })}
+            onClick={() =>
+              !!option.disabled && router.push(option.url ? transformUrl(lng, option.url) : '')
+            }
           >
-            {options.map((option, index) => (
-              <div
-                key={index}
-                className={classNames('w-fit whitespace-nowrap px-5 py-2', {
-                  'text-neutral-400': !option.url || option.disabled,
-                })}
-                onClick={() =>
-                  !!option.disabled && router.push(option.url ? transformUrl(lng, option.url) : '')
-                }
-              >
-                <Suspense fallback={option.label}>{t(option.label)}</Suspense>
-              </div>
-            ))}
-          </PopoverPanel>
-        </div>
-      )}
-    </Popover>
+            {t(option.label)}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
