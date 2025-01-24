36340d6ba452efbb6acb2e63570571d579801100
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2025-01-24 18:00:51 +0000

    fix: moving some types around

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,5 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/studio/[[...index]]/page.tsx` route
- */
-
 import { visionTool } from '@sanity/vision';
-import { defineConfig } from 'sanity';
+import { defineConfig, Preview } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { cloudinarySchemaPlugin } from 'sanity-plugin-cloudinary';
 import { internationalizedArray } from 'sanity-plugin-internationalized-array';
@@ -19,7 +15,13 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    structureTool(),
+    structureTool({
+      name: 'models',
+      title: 'Modelos',
+      defaultDocumentNode: (S) =>
+        S.document().views([S.view.form(), S.view.component(Preview).title('Preview')]),
+    }),
+
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
