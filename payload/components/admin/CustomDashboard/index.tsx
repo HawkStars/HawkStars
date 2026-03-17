340875f0e1995ce0c539d7966983f878157a41ac
Paulo Cardoso <pcardoso.lei@gmail.com>
2026-03-17 23:41:37 +0000

    dashboard back to old one

--- payload/components/admin/CustomDashboard/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DashboardClient from './DashboardClient';
-
-const CustomDashboard: React.FC = () => {
-  return <DashboardClient />;
-};
-
-export default CustomDashboard;
