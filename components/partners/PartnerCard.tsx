579e1e9ecbfe4bf61fe3cd9fc8d6ef1ff37f6a59
Paulo Cardoso <pcardoso.lei@gmail.com>
2026-06-22 23:56:20 +0100

    work on the partner card

--- components/partners/PartnerCard.tsx
+++ components/partners/PartnerCard.tsx
@@ -6,6 +6,36 @@ import { type JSX } from 'react';
 import { Media, Partner } from '@/payload-types';
 import RichText from '@/payload/components/RichText';
 import { FlagIcons } from '@/lib/flags';
+import HawkImage from '../ui/hawk-image';
+
+type PartnerInfoProps = Pick<Partner, 'name' | 'links'>;
+
+const PartnerInfo: React.FC<PartnerInfoProps> = ({ name, links }) => {
+  return (
+    <>
+      <h3 className='mt-auto text-center'>{name}</h3>
+
+      {/* Contacts */}
+      {links && links.length > 0 && (
+        <div className='flex justify-center gap-2'>
+          {links.map((link, index) => {
+            const icon = link && SocialIcon[link.platform as SocialType];
+
+            return (
+              <div key={index}>
+                <Link href={link.url} className='underline' target='_blank'>
+                  {icon && (
+                    <Image src={icon} alt={`${link.platform} icon`} width={24} height={24} />
+                  )}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
 
 const PartnerCard = (partner: Partner): JSX.Element => {
   const { name, description, logo, country, links } = partner;
@@ -27,40 +57,27 @@ const PartnerCard = (partner: Partner): JSX.Element => {
       {/* Image */}
       {url && (
         <div className='flex justify-center pb-4'>
-          <Image
+          <HawkImage
             src={url as string}
             alt={`${name} logo`}
             width={256}
             height={256}
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
             className='aspect-square rounded-t-2xl object-contain object-center px-6'
-            placeholder='blur'
-            blurDataURL={url as string} // TODO: change this to a low quality image placeholder (LQIP) instead of the original image for better performance
           />
         </div>
       )}
 
-      <h3 className='mt-auto text-center'>{name}</h3>
-      {/* Description */}
-      {description && <RichText data={description} />}
-
-      {/* Contacts */}
-      {links && links.length > 0 && (
-        <div className='flex justify-center gap-2'>
-          {links.map((link, index) => {
-            const icon = link && SocialIcon[link.platform as SocialType];
-
-            return (
-              <div key={index}>
-                <Link href={link.url} className='underline' target='_blank'>
-                  {icon && (
-                    <Image src={icon} alt={`${link.platform} icon`} width={24} height={24} />
-                  )}
-                </Link>
-              </div>
-            );
-          })}
+      <div className='flex gap-2 px-4 lg:flex-col'>
+        <div className='lg:hidden'>
+          <PartnerInfo name={name} links={links} />
         </div>
-      )}
+        <div className='flex max-lg:hidden lg:flex-col'>
+          <PartnerInfo name={name} links={links} />
+        </div>
+
+        {description && <RichText data={description} />}
+      </div>
     </div>
   );
 };
