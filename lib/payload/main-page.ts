a12ebf9529269ddb734d2a859e1a62c11bc0cfea
Paulo Cardoso <pcardoso.lei@gmail.com>
2026-09-11 01:40:14 +0100

    cache main page and remove graphql dependency

--- lib/payload/main-page.ts
+++ lib/payload/main-page.ts
@@ -1,8 +1,13 @@
 import { Language } from '@/i18n/settings';
 import { getPayloadConfig } from './server';
+import { cacheLife, cacheTag } from 'next/cache';
+import { MAIN_PAGE_CACHE_TAG } from '@/payload/globals/MainPage/hooks/revalidateMainPage';
 
 const getMainPageInfo = async (lng: Language, opts?: { preview: boolean }) => {
   'use cache';
+  cacheTag(MAIN_PAGE_CACHE_TAG);
+  cacheLife('hours');
+
   const payload = await getPayloadConfig();
   return await payload.findGlobal({
     slug: 'main-page',
